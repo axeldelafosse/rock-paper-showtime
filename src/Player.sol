4ceff8b41267afc564e91042aedce846cd96de00
Axel Delafosse <axeldelafosse@gmail.com>
2022-11-02 15:40:19 +0100

    update strategy

--- src/Player.sol
+++ src/Player.sol
@@ -4,15 +4,27 @@ pragma solidity ^0.8.17;
 import {IPlayer, Throw} from "rock-paper-showtime/src/interfaces/IPlayer.sol";
 
 contract Player is IPlayer {
-    function firstThrow(
-        string calldata /* opponentName */
-    ) external pure returns (Throw) {
-        return Throw.Paper;
+    function firstThrow(string calldata opponentName)
+        external
+        pure
+        returns (Throw)
+    {
+        if (keccak256(bytes(opponentName)) == keccak256("Avalanche")) {
+            return Throw.Rock;
+        } else if (keccak256(bytes(opponentName)) == keccak256("ritz")) {
+            return Throw.Scissors;
+        } else {
+            return Throw.Paper;
+        }
     }
 
-    function nextThrow(
-        Throw /* prevOpponentThrow */
-    ) external pure returns (Throw) {
-        return Throw.Paper;
+    function nextThrow(Throw prevOpponentThrow) external pure returns (Throw) {
+        if (prevOpponentThrow == Throw.Rock) {
+            return Throw.Paper;
+        } else if (prevOpponentThrow == Throw.Paper) {
+            return Throw.Scissors;
+        } else {
+            return Throw.Rock;
+        }
     }
 }
